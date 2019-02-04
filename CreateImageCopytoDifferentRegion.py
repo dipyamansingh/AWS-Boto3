a9c504c156d0111937557c955fe324fb2d9dc910
dipyamansingh <47243551+dipyamansingh@users.noreply.github.com>
2019-02-05 02:34:23 +0530

    Create CreateImageCopytoDifferentRegion.py

---
+++ CreateImageCopytoDifferentRegion.py
@@ -0,0 +1,29 @@
+import boto3
+
+source_region = 'ap-south-1'
+ec2 = boto3.resource('ec2', region_name=source_region)
+
+
+instances = ec2.instances.filter(InstanceIds=['i-0067eeaab6c8198fd'])
+
+image_ids = []
+
+for instance in instances:
+    image = instance.create_image(Name='Demo Deep - '+instance.id, Description='Demo Boto'+instance.id)
+    image_ids.append(image.id)
+
+print("Images to be copied {} ".format(image_ids))
+
+client = boto3.client('ec2', region_name=source_region)
+waiter = client.get_waiter('image_available')
+
+# Wait for Images to be ready
+waiter.wait(Filters=[{
+    'Name': 'image-id',
+    'Values': image_ids
+}])
+
+destination_region = 'us-east-1'
+client = boto3.client('ec2', region_name=destination_region)
+for image_id in image_ids:
+    client.copy_image(Name='Boto3 Copy'+image_id, SourceImageId=image_id, SourceRegion='ap-south-1')
